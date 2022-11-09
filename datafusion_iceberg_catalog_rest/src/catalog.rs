f9304e0806bfc3d511461f8704c713a84983c812
JanKaul <jankaul@mailbox.org>
2022-11-09 17:32:22 +0100

    create test

--- datafusion_iceberg_catalog_rest/src/catalog.rs
+++ datafusion_iceberg_catalog_rest/src/catalog.rs
@@ -8,11 +8,17 @@ use iceberg_rs::catalog::{namespace::Namespace, Catalog};
 
 use crate::schema::DataFusionSchema;
 
-pub struct DatafusionCatalog {
+pub struct DataFusionCatalog {
     catalog: Arc<dyn Catalog>,
 }
 
-impl CatalogProvider for DatafusionCatalog {
+impl DataFusionCatalog {
+    pub fn new(catalog: Arc<dyn Catalog>) -> Self {
+        DataFusionCatalog { catalog }
+    }
+}
+
+impl CatalogProvider for DataFusionCatalog {
     fn as_any(&self) -> &dyn Any {
         self
     }
@@ -40,5 +46,86 @@ impl CatalogProvider for DatafusionCatalog {
         _schema: Arc<dyn SchemaProvider>,
     ) -> Result<Option<Arc<dyn SchemaProvider>>> {
         unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{env, sync::Arc};
+
+    use iceberg_catalog_rest_client::{apis::configuration::Configuration, catalog::RestCatalog};
+    use iceberg_rs::{
+        catalog::Catalog,
+        object_store::{aws::AmazonS3Builder, ObjectStore},
+    };
+
+    use datafusion::{
+        arrow::{array, record_batch::RecordBatch},
+        prelude::*,
+    };
+
+    use super::DataFusionCatalog;
+
+    fn configuration() -> Configuration {
+        Configuration {
+            base_path: "http://localhost:8080".to_string(),
+            user_agent: None,
+            client: reqwest::Client::new(),
+            basic_auth: None,
+            oauth_access_token: None,
+            bearer_access_token: None,
+            api_key: None,
+        }
+    }
+
+    #[tokio::test]
+    pub async fn test_catalog() {
+        let access_key = env::var("AWS_ACCESS_KEY_ID").expect("No access key provided.");
+        let secret_key = env::var("AWS_SECRET_ACCESS_KEY").expect("No secret key provided.");
+
+        let object_store: Arc<dyn ObjectStore> = Arc::new(
+            AmazonS3Builder::new()
+                .with_region("eu-central-1")
+                .with_bucket_name("dashbook-arrow-testing")
+                .with_access_key_id(access_key)
+                .with_secret_access_key(secret_key)
+                .build()
+                .expect("Failed to create aws object store"),
+        );
+
+        let catalog: Arc<dyn Catalog> = Arc::new(RestCatalog::new(
+            "my_catalog".to_owned(),
+            configuration(),
+            "/".to_owned(),
+            object_store,
+        ));
+
+        let datafusion_catalog = Arc::new(DataFusionCatalog::new(catalog));
+
+        let ctx = SessionContext::new();
+
+        ctx.register_catalog("my_catalog", datafusion_catalog);
+
+        let df = ctx
+            .sql("SELECT date, SUM(cases) FROM my_catalog.dashbook.covid_nyt GROUP BY date")
+            .await
+            .expect("Failed to execute query.");
+
+        // execute the plan
+        let results: Vec<RecordBatch> = df.collect().await.expect("Failed to execute query plan.");
+
+        let batch = results
+            .into_iter()
+            .find(|batch| batch.num_rows() > 0)
+            .expect("All record batches are empty");
+
+        let cases = batch
+            .column(1)
+            .as_any()
+            .downcast_ref::<array::Int32Array>()
+            .expect("Failed to get values from batch.");
+
+        // Value can either be 0.9 or 1.8
+        assert_eq!(cases.value(0), 400)
     }
 }
